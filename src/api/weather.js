45c3df47523e9518e5978e1839ed74af3aac6254
rosesarerosieee <perezrose0211@gmail.com>
2024-10-19 17:10:45 +0800

    Hided, the api keys

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,20 +1,24 @@
-const API_KEY = '7c58d942ae9c58bb192c41a4fe76a8b2';
-const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const fetchWeather = async (location) => {
-    const response = await fetch(`${BASE_URL}/weather?q=${location}&appid=${API_KEY}&units=metric`);
-    if (!response.ok) {
-        throw new Error('Failed to fetch the weather data');
-    }
-    const data = await response.json();
-    return data;
+  const response = await fetch(
+    `${BASE_URL}/weather?q=${location}&appid=${API_KEY}&units=metric`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch the weather data");
+  }
+  const data = await response.json();
+  return data;
 };
 
 export const fetchForecast = async (location) => {
-    const response = await fetch(`${BASE_URL}/forecast?q=${location}&appid=${API_KEY}&units=metric`);
-    if (!response.ok) {
-        throw new Error('Failed to fetch the forecast data');
-    }
-    const data = await response.json();
-    return data;
+  const response = await fetch(
+    `${BASE_URL}/forecast?q=${location}&appid=${API_KEY}&units=metric`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch the forecast data");
+  }
+  const data = await response.json();
+  return data;
 };
